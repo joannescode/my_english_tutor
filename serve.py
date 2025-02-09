6d49efc287e7e48e796408febc5ca0b375399330
Lucas Joannes Vieira Ribeiro <joannesribeiro_@outlook.com>
2025-02-09 10:54:19 -0300

    Feat: Py para rodar via browser

---
+++ serve.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from langserve import add_routes
+import src.llm_english_tutor as llm
+
+model = llm.connect_api()
+prompt, history = llm.chatbot_personality()
+chain, _= llm.conversation_with_chatbot(model, prompt, history)
+
+app = FastAPI(title="Meu tutor de inglês", description="Meu tutor de inglês particular desenvolvido com IA.")
+add_routes(app, chain, path="/tutor")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
